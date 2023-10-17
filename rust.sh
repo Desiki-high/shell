4b95393b97488b91fad6f3de1af3a0d7239eec39
Yadong Ding <ding_yadong@foxmail.com>
2023-10-17 19:07:02 +0800

    update rust

--- rust.sh
+++ rust.sh
@@ -6,6 +6,26 @@ RUST_VERSION="1.68.2"
 # Install Rust
 echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain $RUST_VERSION
+
+tee  ~/.cargo/config > /dev/null << EOF
+[source.crates-io]
+registry = "https://github.com/rust-lang/crates.io-index"
+# 指定镜像
+replace-with = 'ustc' # 如：tuna、ustc，或者 rustcc 指定一个即可
+
+# 中国科学技术大学
+[source.ustc]
+registry = "https://mirrors.ustc.edu.cn/crates.io-index"
+
+# 清华大学
+[source.tuna]
+registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
+
+# rustcc社区
+[source.rustcc]
+registry = "https://code.aliyun.com/rustcc/crates.io-index.git"
+EOF
+
 source $HOME/.cargo/env
 rustc --version
 cargo --version
